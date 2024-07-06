feb6f53e0c2ecc348c5da91be88555e12784d2ff
PIYUSH <piyushsangale13@gmail.com>
2024-07-06 20:07:53 +0530

    added rapid api

--- src/RecommendedVideos.js
+++ src/RecommendedVideos.js
@@ -1,80 +1,58 @@
-import React from 'react'
-import './RecommendedVideos.css'
-import VideoCard from './VideosCard'
+import React, { useEffect, useState } from 'react';
+import './RecommendedVideos.css';
+import VideoCard from './VideosCard';
 
+const url = 'https://youtube-v3-alternative.p.rapidapi.com/trending?geo=US&lang=en';
+const options = {
+  method: 'GET',
+  headers: {
+    'x-rapidapi-key': 'ca4a2c1d6dmsh908ef1e8ec1b4ffp12bc29jsn6a05cd068e03',
+    'x-rapidapi-host': 'youtube-v3-alternative.p.rapidapi.com'
+  }
+};
 
 function RecommendedVideos() {
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        if (Array.isArray(result.data)) {
+          setVideos(result.data);
+        } else {
+          console.error('Expected array of videos, received:', result);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className='recommendedVideos'>
-        <h2>Recommended</h2>
-        <div className="recommendedVideos__videos">
-          <VideoCard 
-            title="I Bought iPhone 15 Pro MAX ₹11000 only - Real vs Fake !"
-            views="292K views"
-            timestamp="19 hours ago"
-            channelImage="https://yt3.ggpht.com/ZNfyQU0p0lCWEcgNd9wNeJLr3ln5Eyw9JyOSdNPCht36D4jgVSEPvgIYsGkPyrxTTo4ejRzmuw=s88-c-k-c0x00ffffff-no-rj"
-            channel="Gyan Therapy"
-            image="https://img.youtube.com/vi/F4iyKwHNqk4/mqdefault.jpg"
+      <h2>Recommended</h2>
+      <div className="recommendedVideos__videos">
+        {videos.map((video, index) => (
+          <VideoCard
+            key={video.videoId}
+            title={video.title}
+            views={video.viewCount}
+            timestamp={video.lengthText}
+            channelImage={video.channelThumbnail}
+            channel={video.channelTitle}
+            image={video.thumbnail[0].url}
           />
-          <VideoCard 
-            title="Use Loops With Array in Javascript | Javascript Tutorial in Hindi #15"
-            views="13 views"
-            timestamp="3 days ago"
-            channelImage="https://yt3.ggpht.com/ZehqXHvs8K2kECHQj4hbdJS4Y0_a17Tupu8vwoE5IVXpcBjkjE-WPYVGvz4EAA1dXViFvg8Qyw=s68-c-k-c0x00ffffff-no-rj"
-            channel="The Coding Dev"
-            image="https://img.youtube.com/vi/eZMNzsDUXto/mqdefault.jpg"
-          />
-          <VideoCard 
-            title="Kreo Chimera Wired/Wireless Gaming Mouse"
-            views="1M views"
-            timestamp="27 minutes ago"
-            channelImage="https://yt3.ggpht.com/fjZdGAR2ZaKCXvWgrVwx5t1_IIvNnavCh-W_NDF639b0lK8QkwflaZ3nEaM-WU7I4jWtULhk=s68-c-k-c0x00ffffff-no-rj"
-            channel="Tech Box Hindi"
-            image="https://img.youtube.com/vi/4XiWDk4NA1E/mqdefault.jpg"
-          />
-          <VideoCard 
-            title="Mystery of Flight 571 | World's Greatest Miracle | Dhruv Rathee"
-            views="4.7M views"
-            timestamp="1 month ago"
-            channelImage="https://yt3.ggpht.com/oQ2NQMuq-9IS6_MZdsGXa0RWecaACremA01Z7jo-YpoEF1H6PyUF8PZzXkV10OYwSP3UMJDeTg=s88-c-k-c0x00ffffff-no-rj"
-            channel="Dhruv Rathee"
-            image="https://img.youtube.com/vi/KIvfM4g4aG4/mqdefault.jpg"
-          />
-          <VideoCard 
-            title="I Bought iPhone 15 Pro MAX ₹11000 only - Real vs Fake !"
-            views="292K views"
-            timestamp="19 hours ago"
-            channelImage="https://yt3.ggpht.com/ZNfyQU0p0lCWEcgNd9wNeJLr3ln5Eyw9JyOSdNPCht36D4jgVSEPvgIYsGkPyrxTTo4ejRzmuw=s88-c-k-c0x00ffffff-no-rj"
-            channel="Gyan Therapy"
-            image="https://img.youtube.com/vi/F4iyKwHNqk4/mqdefault.jpg"
-          />
-          <VideoCard 
-            title="Use Loops With Array in Javascript | Javascript Tutorial in Hindi #15"
-            views="13 views"
-            timestamp="3 days ago"
-            channelImage="https://yt3.ggpht.com/ZehqXHvs8K2kECHQj4hbdJS4Y0_a17Tupu8vwoE5IVXpcBjkjE-WPYVGvz4EAA1dXViFvg8Qyw=s68-c-k-c0x00ffffff-no-rj"
-            channel="The Coding Dev"
-            image="https://img.youtube.com/vi/eZMNzsDUXto/mqdefault.jpg"
-          />
-          <VideoCard 
-            title="Kreo Chimera Wired/Wireless Gaming Mouse"
-            views="1M views"
-            timestamp="27 minutes ago"
-            channelImage="https://yt3.ggpht.com/fjZdGAR2ZaKCXvWgrVwx5t1_IIvNnavCh-W_NDF639b0lK8QkwflaZ3nEaM-WU7I4jWtULhk=s68-c-k-c0x00ffffff-no-rj"
-            channel="Tech Box Hindi"
-            image="https://img.youtube.com/vi/4XiWDk4NA1E/mqdefault.jpg"
-          />
-          <VideoCard 
-            title="Mystery of Flight 571 | World's Greatest Miracle | Dhruv Rathee"
-            views="4.7M views"
-            timestamp="1 month ago"
-            channelImage="https://yt3.ggpht.com/oQ2NQMuq-9IS6_MZdsGXa0RWecaACremA01Z7jo-YpoEF1H6PyUF8PZzXkV10OYwSP3UMJDeTg=s88-c-k-c0x00ffffff-no-rj"
-            channel="Dhruv Rathee"
-            image="https://img.youtube.com/vi/KIvfM4g4aG4/mqdefault.jpg"
-          />
-        </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default RecommendedVideos
+export default RecommendedVideos;
--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,90 +1,65 @@
-import React from 'react';
-import "./SearchPage.css";
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import './SearchPage.css';
 import TuneIcon from '@mui/icons-material/Tune';
-import ChannelRow from './ChannelRow';
 import VideoRow from './VideoRow';
 
+const SearchPage = () => {
+  const { searchTerm } = useParams();
+  const [videos, setVideos] = useState([]);
 
-function SearchPage() {
+  useEffect(() => {
+    const fetchData = async () => {
+      const url = `https://youtube-v3-alternative.p.rapidapi.com/search?query=${searchTerm}&geo=US&lang=en`;
+      const options = {
+        method: 'GET',
+        headers: {
+          'x-rapidapi-key': 'ca4a2c1d6dmsh908ef1e8ec1b4ffp12bc29jsn6a05cd068e03',
+          'x-rapidapi-host': 'youtube-v3-alternative.p.rapidapi.com'
+        }
+      };
+
+      try {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const result = await response.json();
+        setVideos(result.data); // Assuming result is an array of videos
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className='searchPage'>
-        <div className="searchPage__filter">
-            <TuneIcon/>
-            <h2>FILTER</h2>
-        </div>
-        <hr />
 
-        <ChannelRow
-            image="https://yt3.ggpht.com/ZNfyQU0p0lCWEcgNd9wNeJLr3ln5Eyw9JyOSdNPCht36D4jgVSEPvgIYsGkPyrxTTo4ejRzmuw=s176-c-k-c0x00ffffff-no-rj-mo"
-            channel ="Gyan Therapy"
-            verified
-            subscriber="2.83M"
-            noOfVideos={382}
-            description="Welcome
-            Subscribe for Tech Videos ⚡️
-            
-            Er.Rakesh 🇮🇳 India
-            
-            Only for business inquiries Contact at-
-            gyantherapyindia@gmail.com
-            "
-        />
+      <h2>Search Results for: {searchTerm}</h2>
+      <div className="searchPage__filter">
+        <TuneIcon />
+        <h2>FILTER</h2>
+      </div>
+      <hr />
 
-        <hr />
-
+      {Array.isArray(videos) && videos.map((video, index) => (
         <VideoRow
-            title="I Bought iPhone 15 Pro MAX ₹11000 only - Real vs Fake !"
-            views="292K views"
-            timestamp="19 hours ago"
-            channelImage="https://yt3.ggpht.com/ZNfyQU0p0lCWEcgNd9wNeJLr3ln5Eyw9JyOSdNPCht36D4jgVSEPvgIYsGkPyrxTTo4ejRzmuw=s88-c-k-c0x00ffffff-no-rj"
-            channel="Gyan Therapy"
-            image="https://img.youtube.com/vi/F4iyKwHNqk4/mqdefault.jpg"
-            description = "I bought Apple iPhone 15 pro MAX at price 11000 in Budget price. In this video i talked about Real iPhone 15 pro vs Fake iPhone 15 Pro MAX "
-
+          key={video.videoId}
+          title={video.title}
+          views={video.viewCount}
+          timestamp={video.publishedText}
+          channelImage={video.channelThumbnail[0].url}
+          channel={video.channelTitle}
+          image={video.thumbnail[0].url}
+          description={video.description}
         />
-        <VideoRow 
-            title="Use Loops With Array in Javascript | Javascript Tutorial in Hindi #15"
-            views="13 views"
-            timestamp="3 days ago"
-            channelImage="https://yt3.ggpht.com/ZehqXHvs8K2kECHQj4hbdJS4Y0_a17Tupu8vwoE5IVXpcBjkjE-WPYVGvz4EAA1dXViFvg8Qyw=s68-c-k-c0x00ffffff-no-rj"
-            channel="The Coding Dev"
-            image="https://img.youtube.com/vi/eZMNzsDUXto/mqdefault.jpg"
-            description="In this video, we are going to learn about,
+      ))}
 
-            Use Loops with Array in Javascript
-            
-            - For Loop
-            - ForEach Loop
-            - Array from Loop
-            - For of Loop
-            - For in Loop"
-          />
-          <VideoRow 
-            title="Kreo Chimera Wired/Wireless Gaming Mouse"
-            views="1M views"
-            timestamp="27 minutes ago"
-            channelImage="https://yt3.ggpht.com/fjZdGAR2ZaKCXvWgrVwx5t1_IIvNnavCh-W_NDF639b0lK8QkwflaZ3nEaM-WU7I4jWtULhk=s68-c-k-c0x00ffffff-no-rj"
-            channel="Tech Box Hindi"
-            image="https://img.youtube.com/vi/4XiWDk4NA1E/mqdefault.jpg"
-            description="🔥 MOUSE SHOWN IN THIS VIDEO (Amazon=🔵Official Website =🔴)
-            🛍️Best Buy Links 👇
-            
-            ⚡️Kreo Chimera Wired (Apply Amazon 5% Discount code at checkout Gift Code = TECHBOX5)
-            🔵https://amzn.to/3PUItcN"
-          />
-          <VideoRow 
-            title="Mystery of Flight 571 | World's Greatest Miracle | Dhruv Rathee"
-            views="4.7M views"
-            timestamp="1 month ago"
-            channelImage="https://yt3.ggpht.com/oQ2NQMuq-9IS6_MZdsGXa0RWecaACremA01Z7jo-YpoEF1H6PyUF8PZzXkV10OYwSP3UMJDeTg=s88-c-k-c0x00ffffff-no-rj"
-            channel="Dhruv Rathee"
-            image="https://img.youtube.com/vi/KIvfM4g4aG4/mqdefault.jpg"
-            description="✏️ JOIN MY TIME MANAGEMENT COURSE: 
-            Learn how to manage time and maximize productivity in my specialised online course. 
-            Join here: https://academy.dhruvrathee.com"
-          />
+      <hr />
     </div>
-  )
-}
+  );
+};
 
-export default SearchPage
+export default SearchPage;
